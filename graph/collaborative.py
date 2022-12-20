4f9a1288f8fb7b7a3d9abffd4065665d1c05d2f8
jonasbarth <jonas.barth.95@gmail.com>
2022-12-20 11:53:27 +0100

    #2 graph setup * create a graph package * add a collaborative module with a method to create a multigraph from various data sources

---
+++ graph/collaborative.py
@@ -0,0 +1,36 @@
+"""Python module for creating the collaborative hero graph."""
+import pandas as pd
+import networkx as nx
+
+
+def create_from(path=None, data=None):
+    """Creates a collaborative hero graph.
+
+    Only specify ONE of the parameters.
+
+    :arg
+    path (str) - the path to a file to create the graph from.
+    data (pd.DataFrame) - a pandas dataframe to create the graph from.
+
+    :return
+    A networkx graph.
+    """
+    if path and data:
+        raise ValueError('You should only specify either path or data, not both.')
+
+    if path:
+        # create from path
+        hero_network = pd.read_csv(path)
+        return _create_graph_from_data(hero_network)
+
+    _create_graph_from_data(data)
+
+
+def _create_graph_from_data(data):
+    graph = nx.MultiGraph()
+    nodes = set(data.hero1.values).union(set(data.hero2.values))
+    edges = zip(data.hero1.values, data.hero2.values)
+    graph.add_nodes_from(nodes)
+    graph.add_edges_from(edges)
+
+    return graph
